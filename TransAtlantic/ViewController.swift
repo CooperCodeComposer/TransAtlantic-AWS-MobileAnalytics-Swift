43983b431099bcc7da16a9f29c96b58844494510
Alistair Cooper <cooper@swiftcodecomposer.com>
2016-11-05 13:17:51 -0700

    Initial Commit

---
+++ TransAtlantic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransAtlantic
+//
+//  Created by Alistair Cooper on 11/5/16.
+//  Copyright © 2016 Alistair Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
