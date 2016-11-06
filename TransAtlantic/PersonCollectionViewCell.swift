0a9a8a37b495c1a1e6b4e93cbcfba2af07e4b68d
Alistair Cooper <cooper@swiftcodecomposer.com>
2016-11-05 21:11:41 -0700

    initial commit

---
+++ TransAtlantic/PersonCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PersonCollectionViewCell.swift
+//  TransAtlantic
+//
+//  Created by Alistair Cooper on 11/5/16.
+//  Copyright © 2016 Alistair Cooper. All rights reserved.
+//
+
+import UIKit
+
+class PersonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ukFlagImageView: UIImageView!
+    @IBOutlet weak var usFlagImageView: UIImageView!
+    @IBOutlet weak var personImageView: UIImageView!
+    
+    
+}
--- TransAtlantic/ViewController.swift
+++ TransAtlantic/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var score: Int = 0
+    
+    // reference to DataSource (model layer)
+    lazy var dataSource: CVDataSource = {
+        return CVDataSource(collectionView: self.collectionView)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collectionView.dataSource = dataSource
+        collectionView.delegate = dataSource
+    }
+    
+    func displayScore() {
+        
+        let ac = UIAlertController(title: "SCORE", message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(ac, animated: true, completion: nil)
+    }
+    
+    func checkAnswers() {
+        
+        // reset score
+        score = 0
+        
+        for person in dataSource.people {
+            
+            guard let guessIsAmerican = person.guessIsAmerican else { return }
+            
+            if person.isAmerican && guessIsAmerican {
+                score += 1
+            }
+            
+            else if !person.isAmerican && !guessIsAmerican {
+                score += 1
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func submit(_ sender: UIButton) {
+        
+        checkAnswers()
+        displayScore()
     }
-
-
 }
+
 
